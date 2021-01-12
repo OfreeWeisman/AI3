5776f4158f821301a6fc1490b8e4d1b275df5a4c
Ofree <ofree21@gmail.com>
2021-01-12 09:22:11 +0200

    knn forest

--- KNNForest.py
+++ KNNForest.py
@@ -1,50 +1,64 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
+import random
+import copy
 import matplotlib.pyplot as plt
 
 
-class ID3:
-    def __init__(self,
-                 decision_tree=None, m=0):
-        self.decision_tree = decision_tree
-        self.m = m
+N = 20
+K = 15
+p = 0.3
 
-    def fit(self, patients):
-        """ This function builds the decision tree according to the training data"""
-        if patients is None:
-            # Create the patients collection from the training data
-            patients = self.tableToPatients('train.csv')
-        # Create the root node of the decision tree
-        root = self.Node(None, None, None, patients, None, None)
-        # Recursively build the tree and update the class field
-        if self.m == 0:
-            self.splitNode(root)
-        else:
-            self.mSplitNode(root, self.m)
-        self.decision_tree = root
 
-    def predict(self, patients):
-        """ This function receives an item to inspect and decides whether the result is 'ill' or 'health' """
-        if patients is None:
-            patients = self.tableToPatients('test.csv')
+class KNNForest:
+    def __init__(self, forest=None, centroids=None):
+        self.forest = forest
+        self.centroids = centroids
+
+    def fit(self):
+        """ This function splits the training data to N different groups
+            For each group, builds a decision tree and centroid vector"""
+
+        forest = []
+        centroids = []
+
+        # split the training data to N groups of patients
+        # For each group built tree and calculate the centroid
+        patients = self.tableToPatients('train.csv')
+        for i in range(N):
+            patients_group = self.randomPatientsGroup(patients, p)
+            tree = self.buildTree(patients_group)
+            centroid = self.calcCentroid(patients_group)
+            forest.append(tree)
+            centroids.append(centroid)
+
+        self.forest = forest
+        self.centroids = centroids
+
+    def predict(self):
+        """ For each patient in the test data, choose K trees from the forest
+            Return the majority's decision
+        """
+
+        patients = self.tableToPatients('test.csv')
         counter = 0
-        for p in patients:
-            if self.diagnose(self.decision_tree, p):
+        # for each patient find K trees with the closest centroid to the patients
+        for patient in patients:
+            committee = self.chooseKTrees(patient.symptoms)
+            sick = 0
+            healthy = 0
+            for tree in committee:
+                if self.diagnose(tree, patient) == 'M':
+                    sick += 1
+                else:
+                    healthy += 1
+
+            decision = 'B' if healthy > sick else 'M'
+            if decision == patient.diagnosis:
                 counter += 1
 
         total = len(patients)
         return counter / total
-
-    def diagnose(self, node, patient):
-        if node.decision is not None:
-            return patient.diagnosis == node.decision
-
-        symptom_val = patient.symptoms[node.feature]
-        if symptom_val < node.threshold:
-            return self.diagnose(node.left_sub_tree, patient)
-        else:
-            return self.diagnose(node.right_sub_tree, patient)
 
     class Node:
         """ This class represents a node of the decision tree.
@@ -57,16 +71,13 @@ class ID3:
                      decision,
                      patients,
                      left_sub_tree,
-                     right_sub_tree,
-                     fathers_patients):
+                     right_sub_tree):
             self.feature = feature
             self.threshold = threshold
             self.decision = decision
             self.patients = patients
             self.left_sub_tree = left_sub_tree
             self.right_sub_tree = right_sub_tree
-            self.fathers_patients = fathers_patients
-
 
     class Patient:
         """ This class represents one example = one line in the table
@@ -78,6 +89,77 @@ class ID3:
                      symptoms):
             self.diagnosis = diagnosis
             self.symptoms = symptoms
+
+    def diagnose(self, node, patient):
+        if node.decision is not None:
+            return node.decision
+
+        symptom_val = patient.symptoms[node.feature]
+        if symptom_val < node.threshold:
+            return self.diagnose(node.left_sub_tree, patient)
+        else:
+            return self.diagnose(node.right_sub_tree, patient)
+
+    def calcCentroid(self, patients):
+        num = len(patients)
+        length = len(patients[0].symptoms)
+        centroid = []
+        for i in range(length):
+            val = 0
+            for patient in patients:
+                val += patient.symptoms[i]
+            centroid.append(val/num)
+        return centroid
+
+    def chooseKTrees(self, vector):
+        k_trees = []
+        my_centroids = copy.deepcopy(self.centroids)
+        for i in range(K):
+            closest = self.closestVector(vector, my_centroids)
+            index = self.getCentroidIndex(closest)
+            k_trees.append(self.forest[index])
+            my_centroids.remove(closest)
+        return k_trees
+
+    def getCentroidIndex(self, c):
+        index = 0
+        for i in range(len(self.centroids)):
+            if self.centroids[i] != c:
+                index += 1
+            else:
+                return index
+
+    def closestVector(self, vector, vectors):
+        min_dist = float('inf')
+        best_v = None
+        for v in vectors:
+            dist = self.distanceBetweenVectors(vector, v)
+            if dist < min_dist:
+                min_dist = dist
+                best_v = v
+        return best_v
+
+    def distanceBetweenVectors(self, v1, v2):
+        diff = [np.abs(v1[i] - v2[i]) for i in range(len(v1))]
+        diff = sum(list(map(lambda x: x*x, diff)))
+        return diff
+
+    def buildTree(self, patients):
+        # Create the root node of the decision tree
+        root = self.Node(None, None, None, patients, None, None)
+        # Recursively build the tree and update the class field
+        self.splitNode(root)
+        return root
+
+    def randomPatientsGroup(self, patients, param):
+        group = []
+        num = int(len(patients) * param)
+        patients_list = list(np.copy(patients))
+        for i in range(num):
+            patient = random.choice(patients_list)
+            group.append(patient)
+            patients_list.remove(patient)
+        return group
 
     def entropy(self, patients):
         sick = 0
@@ -217,92 +299,40 @@ class ID3:
         self.splitNode(node.right_sub_tree)
 
 
-    def majority(self, patients):
-        sick = 0
-        healthy = 0
-        for p in patients:
-            if p.diagnosis == 'M':
-                sick += 1
-            else:
-                healthy += 1
-        if sick >= healthy:
-            return 'M'
-        return 'B'
-
-
-    def mSplitNode(self, node, m):
-        patients = node.patients
-        if len(patients) <= m:
-            node.decision = self.majority(patients)
-            return
-        elif self.allSame(patients, 'M'):
-            node.decision = 'M'
-            return
-        elif self.allSame(patients, 'B'):
-            node.decision = 'B'
-            return
-
-        feature, threshold, smaller, bigger = self.find_feature_and_threshold_to_split_by(patients)
-        left_son = self.Node(None, None, None, smaller, None, None)
-        right_son = self.Node(None, None, None, bigger, None, None)
-        node.left_sub_tree = left_son
-        node.right_sub_tree = right_son
-        node.threshold = threshold
-        node.feature = feature
-
-        self.mSplitNode(node.left_sub_tree, m)
-        self.mSplitNode(node.right_sub_tree, m)
-
-
-def dataToPatients(id3, indices):
-    patients = []
-    data_frame = pd.read_csv('train.csv')
-    table = data_frame.values.tolist()
-    patients_table = [table[i] for i in indices]
-
-    rows = len(patients_table)
-    for i in range(rows):
-        diagnosis = patients_table[i][0]
-        symptoms = patients_table[i]
-        symptoms.pop(0)
-        symptoms = list(map(lambda s: float(s), symptoms))
-        p = id3.Patient(diagnosis, symptoms)
-        patients.append(p)
-    return patients
-
-
-def experiments():
-    kf = KFold(n_splits=5, shuffle=True, random_state=312461270)
-    data_frame = pd.read_csv('train.csv')
-    table = data_frame.values.tolist()
-    parameters = [1, 5, 7, 12, 15, 50, 100, 200]
-    # parameters = [1, 2, 3, 5, 8, 16, 30, 50, 80, 120]
-    # parameters = [2, 5, 7, 12, 30, 80, 120]
-    experiment_results = []
-    for m in parameters:
-        results = 0
-        id3 = ID3(m=m)
-        for train_index, test_index in kf.split(table):
-            patients_for_train = dataToPatients(id3, train_index)
-            patients_for_test = dataToPatients(id3, test_index)
-            id3.fit(patients_for_train)
-            results += id3.predict(patients_for_test)
-        average_success_rate = results / 5
-        experiment_results.append(average_success_rate)
-    print(experiment_results)
-
-    plt.plot(parameters, experiment_results)
-    plt.xlabel('Parameter M')
-    plt.ylabel('Accuracy')
-    plt.show()
-
-
 if __name__ == '__main__':
-    my_id3 = ID3(None)
-    my_id3.fit(None)
-    print(my_id3.predict(None))
+    knn = KNNForest()
+    ns = [5, 10, 15, 20, 50]
+    ks = [3, 5, 7, 11, 21, 33, 45]
+    ps = [0.3, 0.4, 0.5, 0.6, 0.7]
 
-    # my_id3 = ID3(None, 8)
-    # my_id3.fit(None)
-    # print(my_id3.predict(None))
-    # experiments()
+    best_n = None
+    best_k = None
+    best_p = None
+    acc = 0
+
+    for n in ns:
+        for k in ks:
+            if k > n:
+                break
+            N = n
+            K = k
+            # accuracies = []
+            for param in ps:
+                p = param
+                knn.fit()
+                accuracy = knn.predict()
+                # accuracies.append(accuracy)
+                if accuracy > acc:
+                    best_n = n
+                    best_k = k
+                    best_p = param
+                    acc = accuracy
+            print('N=', n, 'K=', k)
+
+            # plt.plot(ps, accuracies)
+            # plt.xlabel('Parameters')
+            # plt.ylabel('Accuracy')
+            # print('N=', n, 'K=', k)
+            # plt.show()
+    print('accuracy=', acc, 'N=', best_n, 'K=', best_k, 'p=', best_p)
+
